f8fa21b6712859d8d21907b91cedf11b168b8989
Brian Thomas <brianraythomas@gmail.com>
2017-10-21 20:43:33 -0600

    report finished

--- ScotchTrackerREST/WebContent/js/report.js
+++ ScotchTrackerREST/WebContent/js/report.js
@@ -9,7 +9,7 @@ function getReportData() {
         dataType: 'json'
     })
     .done(function (data) {
-        console.log(data);
+        // console.log(data);
         var numBottles = 0;
 
         var totalCost = 0;
@@ -29,8 +29,6 @@ function getReportData() {
         var maxPriceName = '';
 
         data.forEach(function (val, idx) {
-            console.log('val.taste: ' + val.taste);
-            console.log('tasteVar: ' + tasteVar);
             if(parseInt(val.taste) < worstTasteVar){
                 worstTasteVar = val.taste;
                 worstTasteName = val.name;
@@ -78,7 +76,7 @@ function buildReport(numBottles, totalCost, avgCost, bestTaste, bestTasteNotes, 
 function buildCard(numBottles, totalCost, avgCost, bestTaste, bestTasteNotes, cheapest, cheapestName, maxPrice, maxPriceName, worstTasteName) {
 /***************Favorite Scotch************************************************/
     var card = $('<div>');
-    card.addClass('card text-white bg-primary mb-3');
+    card.addClass('card text-white bg-primary mb-3 col-sm-6');
 
     var cardHeader = $('<div>');
     cardHeader.addClass('card-header');
@@ -91,17 +89,22 @@ function buildCard(numBottles, totalCost, avgCost, bestTaste, bestTasteNotes, ch
     h4.addClass('card-title');
     h4.text(bestTaste);
 
+    var pMostNotes = $('<p>');
+    pMostNotes.addClass('card-text');
+    pMostNotes.text(bestTasteNotes);
+    pMostNotes.append('<hr />');
+
     var pLeast = $('<p>');
     pLeast.addClass('card-text');
     pLeast.text('Least Favorite: ' + worstTasteName);
 
-    cardBody.append(h4, pLeast);
+    cardBody.append(h4, pMostNotes, pLeast);
     card.append(cardHeader, cardBody);
     $('#content').append(card);
 
     /***************Money Spent************************************************/
     var card2 = $('<div>');
-    card2.addClass('card text-white bg-danger mb-3');
+    card2.addClass('card text-white bg-danger mb-3 col-sm-6');
 
     var cardHeader2 = $('<div>');
     cardHeader2.addClass('card-header');
@@ -114,19 +117,26 @@ function buildCard(numBottles, totalCost, avgCost, bestTaste, bestTasteNotes, ch
     h4Max.addClass('card-title');
     h4Max.text('Total Spent: ' + totalCost);
 
+    var pBottles = $('<p>');
+    pBottles.addClass('card-text');
+    pBottles.text('on ' + numBottles + ' bottles');
+    pBottles.append('<hr />');
+
     var p = $('<p>');
     p.addClass('card-text');
     p.text('Average Price: ' + avgCost);
+    p.append('<hr />');
 
     var pMax = $('<p>');
     pMax.addClass('card-text');
     pMax.text('Most Expensive: ' + maxPriceName +' ($' + maxPrice +')');
+    pMax.append('<hr />');
 
     var pMin = $('<p>');
     pMin.addClass('card-text');
     pMin.text('Lease Expensive: ' + cheapestName +' ($' + cheapest +')');
 
-    cardBody2.append(h4Max, p, pMax, pMin);
+    cardBody2.append(h4Max, pBottles, p, pMax, pMin);
     card2.append(cardHeader2, cardBody2);
     $('#content').append(card2);
 }
